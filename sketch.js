d7e0c351050e32b40556c90831cb089ded38fa3b
Steve <user@Stevens-MBP-2.wifi.pcc.edu>
2020-01-14 10:56:49 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,11 @@
+
+function setup() {
+  // put setup code here
+  createCanvas(1500, 700);
+}
+
+function draw() {
+  // put drawing code here
+  background(100, 20, 0);
+  ellipse(mouseX, mouseY, 200, 200);
+}
