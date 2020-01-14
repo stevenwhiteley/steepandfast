4ee76b1c484110cf522793a7631e4601ee6b2d49
Steven <stevenwhiteley4@gmail.com>
2020-01-14 11:29:55 -0800

    adding pic

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
+PImage bg;
 
 function setup() {
   // put setup code here
   createCanvas(1500, 800);
+  bg = loadImage ("download.jpg");
 }
 
 function draw() {
   // put drawing code here
-  background(400, 30, 200);
+  //background(400, 30, 200);
+  background(bg);
   ellipse(mouseX, mouseY, 200, 200);
 }
