2b909526bb8185b4f06a078b9d57d6cdb48fac28
Steven <stevenwhiteley4@gmail.com>
2020-01-16 11:44:20 -0800

    hey

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
-let bg;
-function preload() {
-	  bg = loadImage ("download.jpg");
-}
+// // let bg;
+// // function preload() {
+// // 	  bg = loadImage ("https://verns.world/ANIblackcat.gif");
+// }
 function setup() {
   // put setup code here
-  createCanvas(1500, 800);
-  image(bg, 500, 500)
+  createCanvas(1000, 1000);
+  //image(bg, 1000, 1000);
 }
 
 function draw() {
   // put drawing code here
   //background(400, 30, 200);
-  background(bg);
+  //background(bg);
   ellipse(mouseX, mouseY, 200, 200);
 }
