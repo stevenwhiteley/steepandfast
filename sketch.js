cb758b29fa7c354b6c865709f9b30074de0c3fa6
Steven <stevenwhiteley4@gmail.com>
2020-01-21 12:30:26 -0800

    colors

--- sketch.js
+++ sketch.js
@@ -18,70 +18,73 @@ function draw(){
      frames++;
   frames = frames % 360;
  
-  push();
+push();
 //translate(mouseX, mouseY); 
 rotateY(rotationAmount*0.5);
 rotateX(rotationAmount*0.5);
+fill(random(255), random(55), random(255), 255);
 box(250, mouseX, mouseY);
-fill(random(55), 55, random(55), 55);
-pop();  
-    
-  
-  push();
+pop();
+
+push();
+  fill(random(255), random(255), random(255), random(255), 255);
+
 ellipse(20, 20, circleY*60, circleX*50);
 //translate (400, 300); 
-pop();  
-  
-  push();
+
+  fill(255, random(255), random(255), random(255), 255);
 ellipse(mouseX, mouseY, circleZ*20, circleX*20);
-fill(255, random(255), random(255), 255);
-//translate (400, 300); 
 pop();
-  
-push();
+//translate (400, 300); 
+
 //translate (width/2, height/2, width/2);
+push();
 beginShape();
   rotateX(rotationAmount*2);
   rotateY(rotationAmount*2);
   vertex(0, 0, 50);
+    //fill(random(255), random(255), random(255), random(255), 255);
   bezierVertex(50, 200, circleZ, circleX, circleY, circleY, circleZ, circleX, circleZ);
 lights();
 endShape(CLOSE);
 pop();
 
-push();
+
 //translate (width/2, height/2, width/2);
+push();
 beginShape();
   rotateX(rotationAmount*2);
   rotateY(rotationAmount*2);
   vertex(0, 0, 50);
+  //fill(random(255), random(255), random(255), random(255), 255);
   bezierVertex(200, circleX, 600, circleX, 400, 800, 257, 200, circleZ);
 lights();
 endShape(CLOSE);
 pop();
 
-push();
 //translate (width/2, height/2, width/2);
+push();
 beginShape();
   rotateX(rotationAmount*2);
   rotateY(rotationAmount*2);
   vertex(0, 0, 50);
+    //fill(random(255), random(255), random(255), random(255), 255);
   bezierVertex(circleY, 500, 300, circleX, 100, 80, 247, 200, circleZ);
 lights();
 endShape(CLOSE);
 pop();
 
-push();
 //translate (width/2, height/2, width/2);
+push();
 beginShape();
   rotateX(rotationAmount*2);
   rotateZ(rotationAmount*2);
   vertex(0, 0, 50);
+    //fill(random(255), random(255), random(255), random(255), 255);
   bezierVertex(300, circleX, 300, circleX, 100, 80, 247, 200, circleZ);
 lights();
 endShape(CLOSE);
 pop();
-
 
 
    circleX = random(-40,40);
