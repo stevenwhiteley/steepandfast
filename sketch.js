2c3ead3147aaa99365a10e3b771aaad7de1fdf62
Steven <stevenwhiteley4@gmail.com>
2020-01-28 17:41:27 -0800

    first commit

--- sketch.js
+++ sketch.js
@@ -1,96 +1,63 @@
-var circleX;
-var circleY;
-var circleZ;
-var frames = 100;
+/*
+ *write a  drawTarget() function makes it easy to draw many distinct 
+ *targets. Each call to drawTarget() should specify the position, size, and number of
+ *rings for each target.
+*/
+// var thing;
+
+// function setup() {
+//   createCanvas(1000, 1000);
+//   background(51);
+//   noStroke();
+//   noLoop();
+// }
+
+// function draw() {
+//   //this code draws one target with 8 rings
+//   let size = width / 10;
+//   let steps = size/8;
+//   let grayvalues = 255/8;
+  
+//   for (i = 0; i < 8; i++) {
+//     fill(i*grayvalues);
+//     ellipse(width/2, height/2, size - i*steps, size - i*steps);
+//   }
+// 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-     background(0);
-
-  circleX = width/10;
-  circleY = height/10;
-  circleZ = width/10;
-  
-}
-
-function draw(){
-    var rotationAmount = radians(frames);
-     frames++;
-  frames = frames % 360;
- 
-push();
-//translate(mouseX, mouseY); 
-rotateY(rotationAmount*0.5);
-rotateX(rotationAmount*0.5);
-fill(random(255), random(255), random(255), 255);
-box(250, mouseX, mouseY);
-pop();
-
-push();
-  fill(random(255), random(255), random(255), random(255), random(255));
-
-ellipse(20, 20, circleY*60, circleX*50);
-//translate (400, 300); 
-
-  fill(255, random(255), random(255), random(255), random(255));
-ellipse(mouseX, mouseY, circleZ*20, circleX*20);
-pop();
-//translate (400, 300); 
-
-//translate (width/2, height/2, width/2);
-push();
-beginShape();
-  rotateX(rotationAmount*2);
-  rotateY(rotationAmount*2);
-  vertex(0, 0, 50);
-    //fill(random(255), random(255), random(255), random(255), 255);
-  bezierVertex(50, 200, circleZ, circleX, circleY, circleY, circleZ, circleX, circleZ);
-lights();
-endShape(CLOSE);
-pop();
-
-
-//translate (width/2, height/2, width/2);
-push();
-beginShape();
-  stroke(random(255), random(255), random(255), random(255));
-    fill(random(255), random(255), random(255), random(255), random(255));
-  rotateX(rotationAmount*2);
-  rotateY(rotationAmount*2);
-  vertex(0, 0, 50);
-  //fill(random(255), random(255), random(255), random(255), 255);
-  bezierVertex(200, circleX, 600, circleX, 400, 800, 257, 200, circleZ);
-lights();
-endShape(CLOSE);
-pop();
-
-//translate (width/2, height/2, width/2);
-push();
-beginShape();
-  rotateX(rotationAmount*2);
-  rotateY(rotationAmount*2);
-  vertex(0, 0, 50);
-    //fill(random(255), random(255), random(255), random(255), 255);
-  bezierVertex(circleY, 500, 300, circleX, 100, 80, 247, 200, circleZ);
-lights();
-endShape(CLOSE);
-pop();
-
-//translate (width/2, height/2, width/2);
-push();
-beginShape();
-  rotateX(rotationAmount*2);
-  rotateZ(rotationAmount*2);
-  vertex(0, 0, 50);
-    //fill(random(255), random(255), random(255), random(255), 255);
-  bezierVertex(300, circleX, 300, circleX, 100, 80, 247, 200, circleZ);
-lights();
-endShape(CLOSE);
-pop();
-
-
-   circleX = random(-40,40);
-    circleY = random(-20,20);
-   circleZ = random(-30,20);
+  createCanvas(720, 400);
+  background(51);
+  noStroke();
+  noLoop();
+  target = new Jitter();
 
 }
+
+function draw() {
+  drawTarget(width * 0.25, height * 0.4, 200, 4);
+  drawTarget(width * 0.5, height * 0.5, 300, 10);
+  drawTarget(width * 0.75, height * 0.3, 120, 6);
+}
+
+function drawTarget(xloc, yloc, size, num) {
+  const grayvalues = 255 / num;
+  const steps = size / num;
+  for (let i = 0; i < num; i++) {
+    fill(i * grayvalues);
+    ellipse(xloc, yloc, size - i * steps, size - i * steps);
+  }
+
+  class Jitter {{
+ constructor() 
+    this.x = random(width);
+    this.y = random(height);
+    this.diameter = random(10, 30);
+    this.speed = 1;
+  }
+}
+move() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  }
+
+
