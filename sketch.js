950a671bcacfda0bc5c26e72e11cccb1e3ad6999
Steven <stevenwhiteley4@gmail.com>
2020-01-31 16:56:26 -0800

    changedcode

--- sketch.js
+++ sketch.js
@@ -1,63 +1,22 @@
-/*
- *write a  drawTarget() function makes it easy to draw many distinct 
- *targets. Each call to drawTarget() should specify the position, size, and number of
- *rings for each target.
-*/
-// var thing;
-
-// function setup() {
-//   createCanvas(1000, 1000);
-//   background(51);
-//   noStroke();
-//   noLoop();
+// // let bg;
+// // function preload() {
+// // 	  bg = loadImage ("https://verns.world/ANIblackcat.gif");
 // }
 
-// function draw() {
-//   //this code draws one target with 8 rings
-//   let size = width / 10;
-//   let steps = size/8;
-//   let grayvalues = 255/8;
-  
-//   for (i = 0; i < 8; i++) {
-//     fill(i*grayvalues);
-//     ellipse(width/2, height/2, size - i*steps, size - i*steps);
-//   }
-// 
+let face;
 
 function setup() {
-  createCanvas(720, 400);
-  background(51);
-  noStroke();
-  noLoop();
-  target = new Jitter();
-
+  // put setup code here
+  createCanvas(1000, 1000, WEBGL);
+  //image(bg, 1000, 1000);
+  face = loadModel('assets/me.obj, true');
 }
 
 function draw() {
-  drawTarget(width * 0.25, height * 0.4, 200, 4);
-  drawTarget(width * 0.5, height * 0.5, 300, 10);
-  drawTarget(width * 0.75, height * 0.3, 120, 6);
+  background(200);
+  scale(0.4); // Scaled to make model fit into canvas
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  normalMaterial(); // For effect
+  model(face);
 }
-
-function drawTarget(xloc, yloc, size, num) {
-  const grayvalues = 255 / num;
-  const steps = size / num;
-  for (let i = 0; i < num; i++) {
-    fill(i * grayvalues);
-    ellipse(xloc, yloc, size - i * steps, size - i * steps);
-  }
-
-  class Jitter {{
- constructor() 
-    this.x = random(width);
-    this.y = random(height);
-    this.diameter = random(10, 30);
-    this.speed = 1;
-  }
-}
-move() {
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
-  }
-
-
